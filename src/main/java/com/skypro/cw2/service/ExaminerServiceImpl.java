36f95e2e45ac3b6fdcc3e818531e089860426f11
krassot1n <krassotin68@gmail.com>
2023-07-17 01:27:18 +0800

    Done

--- src/main/java/com/skypro/cw2/service/ExaminerServiceImpl.java
+++ src/main/java/com/skypro/cw2/service/ExaminerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.skypro.cw2.service;
 
 
+import com.skypro.cw2.exceptions.NotEnoughQuestions;
 import com.skypro.cw2.model.Question;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import java.util.*;
 @Service
 public class ExaminerServiceImpl implements ExaminerService {
     private final List<QuestionService> questionServiceList;
+    private int countQuestionsInServices;
 
     private final Random random = new Random(723);
 
@@ -23,6 +25,10 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     @Override
     public Set<Question> getQuestions(int amount) {
+        countQuestionsInServices = questionServiceList.get(0).getAll().size() +questionServiceList.get(1).getAll().size();
+        if (amount>countQuestionsInServices){
+            throw new NotEnoughQuestions("Не хватает вопросов в сервисах");
+        }
         HashSet<Question> randomQuestion = new HashSet<>();
         while (randomQuestion.size() < amount) {
             if (random.nextInt(0, 2) == 0) {
